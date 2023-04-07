00665f9c58157c18a5e29328044fc95861d45e88
koushik-ruidas <koushikruidas@gmail.com>
2023-04-07 11:34:25 +0530

    changed group id

--- src/main/java/com/techprimers/kafka/springbootkafkaconsumerexample/listener/KafkaConsumer.java
+++ src/main/java/com/techprimers/kafka/springbootkafkaconsumerexample/listener/KafkaConsumer.java
@@ -2,18 +2,20 @@ package com.techprimers.kafka.springbootkafkaconsumerexample.listener;
 
 import com.techprimers.kafka.springbootkafkaconsumerexample.model.User;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "Kafka_Example", group = "group_id")
-    public void consume(String message) {
-        System.out.println("Consumed message: " + message);
-    }
+//    @KafkaListener(topics = "Kafka_Example", group = "group_id")
+//    public void consume(String message) {
+//        System.out.println("Consumed message: " + message);
+//    }
 
+	//group is where kafka listner belongs too.
 
-    @KafkaListener(topics = "Kafka_Example_json", group = "group_json",
+    @KafkaListener(topics = "example", group = "group_json",
             containerFactory = "userKafkaListenerFactory")
     public void consumeJson(User user) {
         System.out.println("Consumed JSON Message: " + user);
