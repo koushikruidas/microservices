f44c4e9d9cde0dfb0861401354dd7e27f46d67d0
koushik-ruidas <koushikruidas@gmail.com>
2023-04-07 11:17:56 +0530

    added requestmapping in MovieCatalog service

--- MovieCatalog/src/main/java/com/microservice/MovieCatalog/controller/CatalogController.java
+++ MovieCatalog/src/main/java/com/microservice/MovieCatalog/controller/CatalogController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microservice.MovieCatalog.model.CatalogItem;
@@ -15,6 +16,7 @@ import com.microservice.MovieCatalog.service.MovieInfoService;
 import com.microservice.MovieCatalog.service.RatingService;
 
 @RestController
+@RequestMapping("/movie")
 public class CatalogController {
 	
 	@Autowired
