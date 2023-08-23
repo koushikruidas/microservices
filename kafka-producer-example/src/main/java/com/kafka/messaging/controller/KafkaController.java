ab47104913a8e2dd6afb307cc8a2f2f5809d038c
Koushik Ruidas <koushikruidas@gmail.com>
2023-08-23 11:28:37 +0530

    changed http method

--- kafka-producer-example/src/main/java/com/kafka/messaging/controller/KafkaController.java
+++ kafka-producer-example/src/main/java/com/kafka/messaging/controller/KafkaController.java
@@ -2,9 +2,7 @@ package com.kafka.messaging.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.kafka.messaging.model.User;
 
@@ -14,7 +12,7 @@ public class KafkaController {
 	@Autowired
 	KafkaTemplate<String, User> kafkaTemplate;
 	private String topic = "example";
-	@GetMapping("/post/{name}")
+	@PostMapping("/post/{name}")
 	public String post(@PathVariable("name") String name) {
 		kafkaTemplate.send(topic, new User(name,"hello@gmail.com"));
 		return "Published Successfully";
